b627669e11cba9bd2047dffa1c8f93e35ea6d6df
John Allenver Belila <belila.johnallenver@gmail.com>
2026-02-18 16:53:09 +0800

    hydration errors fix

--- resources/js/pages/requests/index.tsx
+++ resources/js/pages/requests/index.tsx
@@ -61,7 +61,7 @@ export default function PendingRequests({ requests, page_title }: RequestsPagePr
                                         <h3 className="font-bold">{request.title}</h3>
                                         <p className="mt-2 text-sm">{request.description}</p>
 
-                                        <p className="text-sm mt-4 flex gap-2 items-center">
+                                        <div className="text-sm mt-4 flex gap-2 items-center">
                                             <Avatar size='sm'>
                                                 <AvatarImage
                                                     src='/profile/default.png'
@@ -73,7 +73,7 @@ export default function PendingRequests({ requests, page_title }: RequestsPagePr
                                             <p className="text-xs text-muted-foreground">
                                                 Submitted {moment(request.updated_at).fromNow()}
                                             </p>
-                                        </p>
+                                        </div>
                                     </div>
 
                                     <Link href={route("requests.detail", request.id)} className='flex-0 ml-auto mr-0'>
