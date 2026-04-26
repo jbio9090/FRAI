08d922893e098a294ddc1dda4a63074259e19de7
John Allenver Belila <belila.johnallenver@gmail.com>
2026-04-26 19:12:11 +0800

    fixed mobile for login page

--- resources/js/pages/login.tsx
+++ resources/js/pages/login.tsx
@@ -4,8 +4,8 @@ export default function LoginPage() {
     return (
         <div className="relative min-h-svh overflow-hidden bg-primary">
             <div className="flex flex-col min-h-svh p-6 md:p-10">
-                <div className="flex flex-1 items-center justify-center w-fit mx-auto px-20 py-6 bg-radial from-[#1A41CC] from-43% to-primary to-70%">
-                    <div className="w-full min-w-md max-w-md content border px-12 pt-10 pb-14 rounded-sm bg-white">
+                <div className="flex flex-1 items-center justify-center w-full md:w-fit mx-auto md:px-20 md:py-6 bg-radial from-[#1A41CC] from-43% to-primary to-70%">
+                    <div className="w-full md:min-w-md max-w-md content border px-12 pt-10 pb-14 rounded-sm bg-white">
                         <LoginForm />
                     </div>
                 </div>
