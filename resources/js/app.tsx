42223542a8635424c26b54d61f226832e600a946
John Allenver Belila <belila.johnallenver@gmail.com>
2026-05-11 16:28:03 +0800

    fixed html page name

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -6,7 +6,7 @@ import '../css/app.css';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => (title ? `${title} - ${appName}` : appName),
+    title: (title) => (title ? `${title} - FRAI` : "FRAI"),
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
